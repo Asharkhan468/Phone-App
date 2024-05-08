cf0cd53450df4cd106e3db198bb1470fc6ccb6ce
Ashar Khan <asharullah.khan2468@gmail.com>
2024-05-08 19:36:35 +0500

    "Added phone data to app.js and updated index.html to display phone cards with Bootstrap"
    This commit adds an array of phone objects to app.js, and updates index.html to display each phone as a card using Boot

---
+++ app.js
@@ -0,0 +1,103 @@
+const phones = [
+  {
+    brand: "Samsung",
+    model: "S20",
+    ram: 8,
+    rom: 256,
+    camera: "20 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Xiomi",
+    model: "Note10",
+    ram: 4,
+    rom: 64,
+    camera: "10 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Infinix",
+    model: "Z10",
+    ram: 2,
+    rom: 16,
+    camera: "5 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Tecno",
+    model: "Spark10",
+    ram: 12,
+    rom: 512,
+    camera: "25 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Iphone",
+    model: "14",
+    ram: 4,
+    rom: 1024,
+    camera: "30 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Oppo",
+    model: "F11",
+    ram: 8,
+    rom: 256,
+    camera: "20 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Vivo",
+    model: "Y20",
+    ram: 4,
+    rom: 64,
+    camera: "8 megapixel",
+    price: 15000,
+  },
+  {
+    brand: "Realme",
+    model: "S50",
+    ram: 50,
+    rom: 1024,
+    camera: "60 megapixel",
+    price: 300000,
+  },
+];
+
+
+let div = document.querySelector("#div");
+
+
+for(let i=0 ; i<phones.length; i++){
+    div.innerHTML += `<div class="card" style="width: 18rem;">
+  <div class="card-body bg-dark text-light">
+    <h5 class="card-title">${phones[i].brand + " " + phones[i].model}</h5>
+    <p class="card-text">RAM: ${phones[i].ram + "GB"}</p>
+    <p class="card-text">ROM: ${phones[i].rom + "GB"}</p>
+    <p class="card-text">Camera: ${phones[i].camera}</p>
+    <p class="card-text">Price: ${phones[i].price + " PKR"}</p>
+  <button type="button" class="btn btn-warning funct" onclick="clickFunct(${i})">Add to cart <i class="fa-solid fa-cart-shopping text-secondary"></i></button>
+  </div>
+</div>
+    </div>`;
+
+}
+
+
+
+
+
+// function clickFunct(index){
+//   let cart = [];
+  
+  
+// }
+
+
+
+
+
+
+
+
